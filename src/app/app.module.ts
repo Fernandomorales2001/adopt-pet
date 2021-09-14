f6970d2c2af4e9d69e940a16ad2ec673a7c55a3f
Fernando <fmsepeda2001@gmail.com>
2021-09-13 20:42:03 -0600

    chuchos qleros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,15 +15,13 @@ import { MascotasFormComponent } from './Main/components/mascotas-form/mascotas-
 import { NavigationComponent } from './Main/components/navigation/navigation.component';
 import { AboutUsComponent } from './Main/components/about-us/about-us.component';
 import { LoginComponent } from './Main/components/login/login.component';
-import { ImagePipe } from './main/pipes/image.pipe';
 import { DashboardComponent } from './Main/components/dashboard/dashboard.component';
-<<<<<<< HEAD
+
 import { TasksComponent } from './main/components/tasks/tasks.component';
 import { PrivateTasksComponent } from './main/components/private-tasks/private-tasks.component';
 import { ImagenPipe } from './pipes/imagen.pipe';
-=======
 import { ModalVermasComponent } from './Main/Models/modal-vermas/modal-vermas.component';
->>>>>>> acdce33899c8069021d74184617fc7efe0dc25b1
+
 
 
 @NgModule({
@@ -35,15 +33,11 @@ import { ModalVermasComponent } from './Main/Models/modal-vermas/modal-vermas.co
     NavigationComponent,
     AboutUsComponent,
     LoginComponent,
-    ImagePipe,
     DashboardComponent,
-<<<<<<< HEAD
     TasksComponent,
     PrivateTasksComponent,
-    ImagenPipe
-=======
+    ImagenPipe,
     ModalVermasComponent
->>>>>>> acdce33899c8069021d74184617fc7efe0dc25b1
     ],
   imports: [
     BrowserModule,
